863e982681aaa567d97b5430b8ef107f31fb026b
sugarkane <hrefel@gmail.com>
2021-11-17 07:27:54 +0700

    add routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,22 @@
 import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import About from "./pages/About/About.page";
+import Home from "./pages/Home/Home.page";
 
-const App: React.FC<{}> = () => {
-    return(
-        <h1>HAI</h1>
-    )
+export default class App extends React.Component {
+  render() {
+    return (
+      <Router>
+        <div>
+          <h1>Welcome to My Ap</h1>
+        
+          <Routes>
+            <Route path="/" element={<Home />}></Route>
+            <Route path="/about" element={<About />}>
+            </Route>
+          </Routes>
+        </div>
+      </Router>
+    );
+  }
 }
-
-export default App;
